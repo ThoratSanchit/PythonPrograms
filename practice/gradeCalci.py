9598b56e8e619a5629bf0d17a6ce82d8747269c0
ThoratSanchit <sanchitcode77@gmail.com>
2024-02-02 20:35:30 +0530

    Some file changes

--- practice/gradeCalci.py
+++ practice/gradeCalci.py
@@ -1,26 +1,27 @@
-print("This is the grade calculator ")
+print("--------------------This is the grade calculator------------- ")
 marks=[]
 sum=0
 def gradeCalculator():
-    print("Enter the 6 subject marks:")
+   
     for i in range(1,7):
+        print("Enter the subject {} mark ".format(i))
         marks.append(int(input()));
        
    
 gradeCalculator()    
 for e in marks:
     sum+=e
-print("Sum", sum)
+
 percentage=sum/6  
 print(percentage)
 if percentage>=70:
     print("You pass with first class.")
 elif percentage>50 and percentage<70:
-    print("you pass with second class.")  
+    print("You are pass with second class.")  
 elif percentage>=35 and percentage<50:
-    print("you pass")    
+    print("You are pass")    
 else:
-    print("you are fail")
+    print("Sorry you are fail")
      
 
 
